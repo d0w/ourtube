84e44da00633ae96d22189ad901d7df219da84ac
Derek <derekxu888@gmail.com>
2023-01-23 20:51:51 -0500

    Some video routes

--- backend/controllers/video.js
+++ backend/controllers/video.js
@@ -86,14 +86,15 @@ export const getByTags = async (req, res, next) => {
     const tags = req.query.tags.split(",");
     console.log(tags);
     try {
-           
+           const videos = await Video.find({ tags: { $in: tags }}).limit(20);
     } catch (error) {
         next(error);
     }
 }
 export const search = async (req, res, next) => {
+    const query = req.query.q;
     try {
-        const videos = await Video.find().sort({ views: -1 });
+        const videos = await Video.find({ title: { $regex: query, $options: "i" }}).limit(40);
         res.status(200).json(videos);    
     } catch (error) {
         next(error);
