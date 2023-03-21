7a33cf23731685f59bbb26d8cb0d08456db0e38d
Derek <derekxu888@gmail.com>
2023-03-20 23:00:05 -0400

    fuck

--- ourtube/src/index.js
+++ ourtube/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from '../redux/store';
+import { store } from './redux/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}>
       <App />
-    </Provider>
+    </Provider> */}
+    <App />//
 
   </React.StrictMode>
 );
