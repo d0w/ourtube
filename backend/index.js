ca24f990a16e748fa0f039fa429f9ac09f2d7281
Derek <derekxu888@gmail.com>
2023-04-11 13:26:08 -0400

    pre merge

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import commentRoutes from "./routes/comments.js";
 import videoRoutes from "./routes/videos.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser"
-import cors from 'cors'
+//import cors from 'cors'
 
 const app = express();
 dotenv.config()
